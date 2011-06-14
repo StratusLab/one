f63a846d7d1deb8268534a880b1b06171d7d2a7f
Konstantin Skaburskas <konstan@sixsq.com>
2011-06-14 11:25:38 +0200

    Export context environment variables directly within memory.

--- opennebula/tar/src/main/scripts/init.sh
+++ opennebula/tar/src/main/scripts/init.sh
@@ -1,20 +1,19 @@
 #!/bin/sh -e
 
-. /mnt/stratuslab/context.sh
+CONTEXT_FILE=/mnt/stratuslab/context.sh
+
+. $CONTEXT_FILE
 
 mkdir -p /root/.ssh
 echo "$PUBLIC_KEY" > /root/.ssh/authorized_keys
 chmod -R 600 /root/.ssh/
 
 if [ -n "$SCRIPT_EXEC" ]; then
-    CONTEXT=$(mktemp)
-    cp -f /mnt/stratuslab/context.sh $CONTEXT
-    sed -i -e '/^[A-Z]*=/s|^|export |' $CONTEXT
-    . $CONTEXT
-    rm -f $CONTEXT
+    VARS=`cat $CONTEXT_FILE | egrep -e '^[a-zA-Z\-\_0-9]*=' | sed 's/=.*$//'`
+    for v in $VARS; do
+      export $v
+    done
     INIT_SCRIPT=/tmp/init_extra.sh
     echo $SCRIPT_EXEC > $INIT_SCRIPT
     sh $INIT_SCRIPT
-else
-    touch /tmp/stratuslab-context-no-SCRIPT_EXEC
 fi
