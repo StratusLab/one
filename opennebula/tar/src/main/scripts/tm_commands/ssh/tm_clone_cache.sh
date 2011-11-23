503ff99d6106d42b43a2f874dbb763a0ff43d56c
Konstantin Skaburskas <Konstantin.Skaburskas@gmail.com>
2011-11-23 20:49:33 +0100

    Added a possibility to boot directly from pdisk ID bypassing policy.

--- opennebula/tar/src/main/scripts/tm_commands/ssh/tm_clone_cache.sh
+++ opennebula/tar/src/main/scripts/tm_commands/ssh/tm_clone_cache.sh
@@ -33,157 +33,195 @@ DST_PATH=`arg_path $DST`
 SRC_HOST=`arg_host $SRC`
 DST_HOST=`arg_host $DST`
 
-log "Applying policy on $SRC"
-IMAGEID=${SRC##*/}
-
-# Retrieve the first fully qualified validated MP identifier 
-output=
-exec_and_log "stratus-policy-image $SRC" "Failed policy validation" true
-IDENTIFIER=$output
-
 PDISKPORT=8445
 export STRATUSLAB_PDISK_ENDPOINT=$(stratus-config persistent_disk_ip)
 
-VGPATH=$(stratus-config persistent_disk_lvm_device)
-TMPSTORE=/tmp
+function start_from_cow_snapshot() {
 
-log "Get PDISK ID from cache."
-# NB! search may return more than one PDISK ID as the tag in not unique!
-PDISKID=$(stratus-storage-search tag $IMAGEID) 
-if [ "$?" -eq "0" ];then 
-    if [ -z "$PDISKID" ]; then
-        log "Cache miss. Image $IMAGEID not cached."
-        log "Download and cache it."
-
-        IMAGELOCATION=$(stratus-manifest --get-element location $IDENTIFIER)
-
-        # Critical section - start.
-        # Image and its stored copy have to be deleted if something goes wrong.
-        # See blow fo the end of the critical section.
-        PDISKID=
-        function onexit() {
-            $SSH -t -t $STRATUSLAB_PDISK_ENDPOINT rm -f $IMAGE_LOCAL
-            [ -n "$PDISKID" ] && stratus-storage-delete $PDISKID
-        }
-        trap onexit EXIT
-
-        set -e
-
-        IMAGE_LOCAL=$TMPSTORE/$(date +%s).${IMAGELOCATION##*/}
-        exec_and_log "$SSH -t -t $STRATUSLAB_PDISK_ENDPOINT curl -o $IMAGE_LOCAL $IMAGELOCATION" \
-            "Failed to download $IMAGELOCATION" true
-
-        IMAGECOMPRESS=$(stratus-manifest --get-element compression $IDENTIFIER)
-        uncompress=
-        if [ "$IMAGECOMPRESS" = "gz" ];then 
-            uncompress=/bin/gunzip
-        elif [ "$IMAGECOMPRESS" = "bz2" ]; then
-            uncompress=/bin/bunzip2 
-        fi
-
-        IMAGESIZE_b=$(stratus-manifest --get-element bytes $IDENTIFIER)
-        IMAGEFORMAT=$(stratus-manifest --get-element format $IDENTIFIER)
-
-        log "OK - IMAGEID: $IMAGEID. Location: $IMAGELOCATION. Compression: $IMAGECOMPRESS"
-        log "Create logical volume for the new disk."
-        
-        if [[ $IMAGEFORMAT == qco* ]]; then
-            if [ ! -z $uncompress ] ; then
-                $uncompress $IMAGE_LOCAL
-                IMAGE_LOCAL=${IMAGE_LOCAL%.*}
+    log "Applying policy on $SRC"
+    IMAGEID=${SRC##*/}
+    
+    # Retrieve the first fully qualified validated MP identifier 
+    output=
+    exec_and_log "stratus-policy-image $SRC" "Failed policy validation" true
+    IDENTIFIER=$output
+    
+    VGPATH=$(stratus-config persistent_disk_lvm_device)
+    TMPSTORE=/tmp
+    
+    log "Get PDISK ID from cache."
+    # NB! search may return more than one PDISK ID as the tag in not unique!
+    PDISKID=$(stratus-storage-search tag $IMAGEID) 
+    if [ "$?" -eq "0" ];then 
+        if [ -z "$PDISKID" ]; then
+            log "Cache miss. Image $IMAGEID not cached."
+            log "Download and cache it."
+    
+            IMAGELOCATION=$(stratus-manifest --get-element location $IDENTIFIER)
+    
+            # Critical section - start.
+            # Image and its stored copy have to be deleted if something goes wrong.
+            # See blow fo the end of the critical section.
+            PDISKID=
+            function onexit() {
+                $SSH -t -t $STRATUSLAB_PDISK_ENDPOINT rm -f $IMAGE_LOCAL
+                [ -n "$PDISKID" ] && stratus-storage-delete $PDISKID
+            }
+            trap onexit EXIT
+    
+            set -e
+    
+            IMAGE_LOCAL=$TMPSTORE/$(date +%s).${IMAGELOCATION##*/}
+            exec_and_log "$SSH -t -t $STRATUSLAB_PDISK_ENDPOINT curl -o $IMAGE_LOCAL $IMAGELOCATION" \
+                "Failed to download $IMAGELOCATION" true
+    
+            IMAGECOMPRESS=$(stratus-manifest --get-element compression $IDENTIFIER)
+            uncompress=
+            if [ "$IMAGECOMPRESS" = "gz" ];then 
+                uncompress=/bin/gunzip
+            elif [ "$IMAGECOMPRESS" = "bz2" ]; then
+                uncompress=/bin/bunzip2 
             fi
-            IMAGESIZE_b=$(qemu-img info $IMAGE_LOCAL | awk '/virtual size/ {sub(/\(/,""); print $4}' )
-        elif [ "$IMAGEFORMAT" = "raw" ]; then
-            if [ "$IMAGECOMPRESS" = "bz2" ]; then
-                IMAGESIZE_b=$(bzcat $IMAGE_LOCAL | wc -c)
-            elif [ "$IMAGECOMPRESS" = "gz" ]; then
-                #IMAGESIZE_b=$(gunzip -l $IMAGE_LOCAL | tail -1 | awk '{print $2}')
+    
+            IMAGESIZE_b=$(stratus-manifest --get-element bytes $IDENTIFIER)
+            IMAGEFORMAT=$(stratus-manifest --get-element format $IDENTIFIER)
+    
+            log "OK - IMAGEID: $IMAGEID. Location: $IMAGELOCATION. Compression: $IMAGECOMPRESS"
+            log "Create logical volume for the new disk."
+            
+            if [[ $IMAGEFORMAT == qco* ]]; then
+                if [ ! -z $uncompress ] ; then
+                    $uncompress $IMAGE_LOCAL
+                    IMAGE_LOCAL=${IMAGE_LOCAL%.*}
+                fi
+                IMAGESIZE_b=$(qemu-img info $IMAGE_LOCAL | awk '/virtual size/ {sub(/\(/,""); print $4}' )
+            elif [ "$IMAGEFORMAT" = "raw" ]; then
+                if [ "$IMAGECOMPRESS" = "bz2" ]; then
+                    IMAGESIZE_b=$(bzcat $IMAGE_LOCAL | wc -c)
+                elif [ "$IMAGECOMPRESS" = "gz" ]; then
+                    #IMAGESIZE_b=$(gunzip -l $IMAGE_LOCAL | tail -1 | awk '{print $2}')
+    
+                    #PART_INFO=$(zcat $IMAGE_LOCAL | file -)
+                    #IMAGESIZE_b=$(echo $PART_INFO | awk ... 
+    
+                    IMAGESIZE_b=$IMAGESIZE_b
+                fi 
+            fi
+    
+            #IMAGESIZE_G=$(echo "scale=3; $IMAGESIZE_b/1024^3" | bc)
+            IMAGESIZE_G=$(echo "$IMAGESIZE_b/1024^3 + 1" | bc)
+    
+            # This new LV should never be shared by iSCSI server.
+            PDISKID=$(stratus-storage -s $IMAGESIZE_G | cut -d' ' -f 2)
+    
+            if [[ $IMAGEFORMAT == qco* ]]; then
+                # qcow image can be put on LV as is w/o conversion to raw. Test this.
+                # Though this probably will not work with create-image.
+                exec_and_log "$SSH -t -t $STRATUSLAB_PDISK_ENDPOINT sudo qemu-img convert -O raw $IMAGE_LOCAL $VGPATH/$PDISKID" \
+                    "Failed to convert qcow image to raw." true
+            else
+                exec_and_log "$SSH -t -t $STRATUSLAB_PDISK_ENDPOINT sh -c \"$uncompress -c $IMAGE_LOCAL | sudo /bin/dd of=$VGPATH/$PDISKID bs=2048\"" \
+                    "Failed to uncompress the image to Logical Volume" true
+            fi
+    
+            set +e
+    
+            trap - EXIT
+            # Critical section - end.
+    
+            # Define tag for the base image (origin)
+            exec_and_log "stratus-storage-update $PDISKID tag $IMAGEID" \
+                 "Failed updating tag for $PDISKID disk" true
 
-                #PART_INFO=$(zcat $IMAGE_LOCAL | file -)
-                #IMAGESIZE_b=$(echo $PART_INFO | awk ... 
-
-                IMAGESIZE_b=$IMAGESIZE_b
-            fi 
+		    exec_and_log "stratus-storage-update $PDISKID isreadonly true" \
+		         "Failed updating the disk storage" true
         fi
-
-        #IMAGESIZE_G=$(echo "scale=3; $IMAGESIZE_b/1024^3" | bc)
-        IMAGESIZE_G=$(echo "$IMAGESIZE_b/1024^3 + 1" | bc)
-
-        # This new LV should never be shared by iSCSI server.
-        PDISKID=$(stratus-storage -s $IMAGESIZE_G | cut -d' ' -f 2)
-
-        if [[ $IMAGEFORMAT == qco* ]]; then
-            # qcow image can be put on LV as is w/o conversion to raw. Test this.
-            # Though this probably will not work with create-image.
-            exec_and_log "$SSH -t -t $STRATUSLAB_PDISK_ENDPOINT sudo qemu-img convert -O raw $IMAGE_LOCAL $VGPATH/$PDISKID" \
-                "Failed to convert qcow image to raw." true
-        else
-            exec_and_log "$SSH -t -t $STRATUSLAB_PDISK_ENDPOINT sh -c \"$uncompress -c $IMAGE_LOCAL | sudo /bin/dd of=$VGPATH/$PDISKID bs=2048\"" \
-                "Failed to uncompress the image to Logical Volume" true
-        fi
-
-        set +e
-
-        trap - EXIT
-        # Critical section - end.
-
-        # Define tag for the base image (origin)
-        exec_and_log "stratus-storage-update $PDISKID tag $IMAGEID" \
-             "Failed updating tag for $PDISKID disk" true
+    else
+        log "Failed to get PDISK ID for image id: $IMAGEID"
+        exit 1
     fi
-else
-    log "Failed to get PDISK ID for image id: $IMAGEID"
-    exit 1
-fi
-log "PDISK ID $PDISKID for IMAGEID $IMAGEID"
-
-# Critical section - start.
-# Delete COW snapshot on any failure.
-PDISKID_COW=
-function onexit() {
-    [ -n "$PDISKID_COW" ] && stratus-storage-delete $PDISKID_COW
+    log "PDISK ID $PDISKID for IMAGEID $IMAGEID"
+    
+    # Critical section - start.
+    # Delete COW snapshot on any failure.
+    PDISKID_COW=
+    function onexit() {
+        [ -n "$PDISKID_COW" ] && stratus-storage-delete $PDISKID_COW
+    }
+    trap onexit EXIT
+    
+    set -e
+    
+    log "Requesting snapshot disk of the origin: $PDISKID"
+    output=
+    exec_and_log "stratus-storage --cow $PDISKID -t $IMAGEID" \
+        "Failed to create snapshot of origin $PDISKID" true
+    PDISKID_COW=$(echo $output | cut -d' ' -f 2)
+    # Define tag for the snapshot as the above -t doesn't work
+    exec_and_log "stratus-storage-update $PDISKID_COW tag snapshot:$PDISKID" \
+         "Failed updating the disk storage" true
+    log "Snapshot disk created: $PDISKID_COW"
+    
+    INSTANCEID=$(basename $(dirname $(dirname $DST_PATH)))
+    log "Get instance owner."
+    USER=$(onevm list | awk '/^[ \t]*'$INSTANCEID' / {print $2}')
+    exec_and_log "stratus-storage-update $PDISKID_COW owner $USER" \
+         "Failed updating the disk storage" true
+    
+    PDISKID_COW_URL=pdisk:$STRATUSLAB_PDISK_ENDPOINT:$PDISKPORT:$PDISKID_COW
+    
+    DST_DIR=`dirname $DST_PATH`
+    
+    log "creating directory $DST_DIR"
+    exec_and_log "$SSH -t -t $DST_HOST mkdir -p $DST_DIR" \
+        "error creating directory $DST_DIR" true
+    
+    log "Persistent disk handling $PDISKID_COW_URL $DST"
+    exec_and_log "$SSH -t -t $DST_HOST /usr/sbin/attach-persistent-disk.sh $PDISKID_COW_URL $DST_PATH" \
+        "Failed to attach persistent disk $DST_PATH" true
+    
+    set +e
+    
+    trap - EXIT
+    # Critical section - end.
 }
-trap onexit EXIT
 
-set -e
+function start_from_persisted() {
+    set -x
+    INSTANCEID=$(basename $(dirname $(dirname $DST_PATH)))
+    USER=$(onevm list | awk '/^[ \t]*'$INSTANCEID'/ {print $2}')
+    PASS=$(awk -F= '/'$USER'/, sub(/\,.*/,"") {print $2}' /etc/stratuslab/authn/login-pswd.properties)
 
-log "Requesting snapshot disk of the origin: $PDISKID"
-output=
-exec_and_log "stratus-storage --cow $PDISKID -t $IMAGEID" \
-    "Failed to create snapshot of origin $PDISKID" true
-PDISKID_COW=$(echo $output | cut -d' ' -f 2)
-# Define tag for the snapshot as the above -t doesn't work
-exec_and_log "stratus-storage-update $PDISKID_COW tag snapshot:$PDISKID" \
-     "Failed updating the disk storage" true
-log "Snapshot disk created: $PDISKID_COW"
+    UUID=${SRC##*:}
+    COW_FALSE=$(stratus-storage-search --pdisk-username $USER --pdisk-password $PASS iscow false)
+    READONLY_FALSE=$(stratus-storage-search --pdisk-username $USER --pdisk-password $PASS isreadonly false)
+    if ( echo $COW_FALSE | grep -q $UUID ) && ( echo $READONLY_FALSE | grep -q $UUID ); then
+        DST_DIR=`dirname $DST_PATH`
+        log "creating directory $DST_DIR"
+        exec_and_log "$SSH -t -t $DST_HOST mkdir -p $DST_DIR" \
+            "error creating directory $DST_DIR" true
 
-INSTANCEID=$(basename $(dirname $(dirname $DST_PATH)))
-log "Get instance owner."
-USER=$(onevm list | awk '/^[ \t]*'$INSTANCEID' / {print $2}')
-exec_and_log "stratus-storage-update $PDISKID_COW owner $USER" \
-     "Failed updating the disk storage" true
+        log "Persistent disk handling $SRC $DST"
+        exec_and_log "$SSH -t -t $DST_HOST /usr/sbin/attach-persistent-disk.sh $SRC $DST_PATH" \
+            "Failed to clone persistent disk $DST_PATH"
+    else
+        exec_and_log "/bin/false" \
+            "Failed booting from $SRC. Check that you are the owner of the disk, the disk is not readonly and not a snapshot."
+    fi
+    set +x
+}
 
-exec_and_log "stratus-storage-update $PDISKID isreadonly true" \
-     "Failed updating the disk storage" true
-
-PDISKID_COW_URL=pdisk:$STRATUSLAB_PDISK_ENDPOINT:$PDISKPORT:$PDISKID_COW
-
-DST_DIR=`dirname $DST_PATH`
-
-log "creating directory $DST_DIR"
-exec_and_log "$SSH -t -t $DST_HOST mkdir -p $DST_DIR" \
-    "error creating directory $DST_DIR" true
-
-log "Persistent disk handling $PDISKID_COW_URL $DST"
-exec_and_log "$SSH -t -t $DST_HOST /usr/sbin/attach-persistent-disk.sh $PDISKID_COW_URL $DST_PATH" \
-    "Failed to attach persistent disk $DST_PATH" true
-
-set +e
-
-trap - EXIT
-# Critical section - end.
+case $SRC in
+    pdisk:*)
+        start_from_persisted
+        ;; 
+    *) 
+        start_from_cow_snapshot
+        ;;
+esac
 
 if [ ! -L $DST_PATH ]; then
   exec_and_log "$SSH -t -t $DST_HOST sudo /bin/chmod ug+w,o-rwx $DST_PATH" \
       "Failed to change mode of $DST_PATH" true
 fi
+
