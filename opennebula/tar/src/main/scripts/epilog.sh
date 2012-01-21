40beeeefb6c42d0949650dc2ede8f30c83894c95
Konstantin Skaburskas <Konstantin.Skaburskas@gmail.com>
2012-01-21 15:37:40 +0100

    added missing files

---
+++ opennebula/tar/src/main/scripts/epilog.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+
+CONTEXT_FILE=context.sh
+
+. $CONTEXT_FILE
+
+mkdir -p /root/.ssh
+echo "$PUBLIC_KEY" > /root/.ssh/authorized_keys
+chmod -R 600 /root/.ssh/
+
+if [ -n "$SCRIPT_EXEC" ]; then
+    VARS=`cat $CONTEXT_FILE | egrep -e '^[a-zA-Z\-\_0-9]*=' | sed 's/=.*$//'`
+    for v in $VARS; do
+      export $v
+    done
+    INIT_SCRIPT=/tmp/init_extra.sh
+    echo $SCRIPT_EXEC > $INIT_SCRIPT
+    sh $INIT_SCRIPT
+fi
